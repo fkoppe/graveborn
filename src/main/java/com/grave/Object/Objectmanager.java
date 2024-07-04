a20041b4f6c9f18378c00336327832b3ae699b6a
Felix Koppe <fkoppe@web.de>
2024-07-04 11:16:26 +0200

    Add Objectmanager

---
+++ src/main/java/com/grave/Object/Objectmanager.java
@@ -0,0 +1,21 @@
+package com.grave.Object;
+
+public class Objectmanager {
+
+    public Objectmanager()
+    {
+        
+    }
+
+    public void init() {
+
+    }
+
+    public void shutdown() {
+
+    }
+
+    public void update() {
+
+    }
+}
