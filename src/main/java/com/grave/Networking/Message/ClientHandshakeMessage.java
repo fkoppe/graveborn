bd66ea8af0b0b70338cb96e1d098e738566585fa
Felix Koppe <fkoppe@web.de>
2024-07-24 10:16:46 +0200

    Unfinished work

--- src/main/java/com/grave/Networking/Message/ClientHandshakeMessage.java
+++ src/main/java/com/grave/Networking/Message/ClientHandshakeMessage.java
@@ -1,30 +1,23 @@
 package com.grave.Networking.Message;
 
 import com.jme3.network.serializing.Serializable;
-import com.grave.Object.Update;
 import com.jme3.network.AbstractMessage;
 
 @Serializable
 public class ClientHandshakeMessage extends AbstractMessage {
     private final String clientName;
-    private final Update all;
+    //private final Update all;
 
     public ClientHandshakeMessage() {
         clientName = null;
-        all = null;
     }
 
-    public ClientHandshakeMessage(String clientName_, Update all_)
+    public ClientHandshakeMessage(String clientName_)
     {
         clientName = clientName_;
-        all = all_;
     }
 
     public String getClientName() {
         return clientName;
-    }
-
-    public Update getAll() {
-        return all;
     }
 }
--- src/main/java/com/grave/Networking/NetClientStateListener.java
+++ src/main/java/com/grave/Networking/NetClientStateListener.java
@@ -18,7 +18,7 @@ public class NetClientStateListener implements ClientStateListener {
     }
 
     public void clientConnected(Client instance) {
-        Message message = new ClientHandshakeMessage(client.getName(), client.allLocal);
+        Message message = new ClientHandshakeMessage(client.getName());
         instance.send(message);
     }
 
